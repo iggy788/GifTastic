0b079ac066b263292f0b48b0c290b6f73fdc76ae
Mike <mike@Mike-Ignaczak-MacBook-Pro.local>
2018-01-02 21:26:18 -0600

    cleaning up JS

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,32 +1,19 @@
 $('document').ready(function() {
-    var players = [
-        'Michael Jordan',
-        'Ervin Magic Johnson',
-        'LeBron James',
-        'Larry Bird',
-        'Kobe Bryant',
-        'Kareem Abdul Jabbar',
-        'Wilt Chamberlain',
-        'Bill Russell',
-        "Shaquille O'Neal",
-        'Oscar Robertson',
-    ];
+    var players = ['Michael Jordan', 'Ervin Magic Johnson', 'LeBron James', 'Larry Bird', 'Kobe Bryant', 'Kareem Abdul Jabbar', 'Wilt Chamberlain', 'Bill Russell', "Shaquille O'Neal", 'Oscar Robertson'];
 
-    // Function for displaying players
+    /* THE FUNCTION renderButtons(); WILL CREATE NEW BUTTONS FOR EVERY PLATER IN THE ABOVE ARRAY */
+
     function renderButtons() {
         // Deleting the player buttons prior to adding new player buttons
 
         $('#players-view').empty();
 
-
         for (var i = 0; i < players.length; i++) {
-
             var a = $('<button>');
 
             a.addClass('player');
 
             a.attr('data-name', players[i]);
-
 
             a
                 .text(i + 1 + '. ' + players[i])
@@ -34,98 +21,88 @@ $('document').ready(function() {
                 .css('background-color', 'black')
                 .css('font-size', '25px');
 
-
             $('#players-view').append(a);
         }
     }
-    /* ADDING PLAYER BUTTONS WHEN NEW ONES ARE ENTERED IN TEXT BOX */
 
-    $('#add-player').on('click', function(event) {
+    /* CALLING renderButtons(); HERE WILL MAKE SURE THE PLAYER'S IN THE ARRAY HAVE BUTTONS CREATED */
 
-        event.preventDefault();
-
-
-        var player = $('#player-input')
-            .val()
-            .trim();
-
-        players.push(player);
-
-
-        renderButtons();
-    });
-
-    /* ADDING PLAYER BUTTONS WHEN NEW ONES ARE ENTERED IN TEXT BOX */
     renderButtons();
 
-    $('#add-player').on('click', function(event) {
-        event.preventDefault();
+    /* ADDING PLAYER BUTTONS WHEN NEW ONES ARE ENTERED IN TEXT BOX */
 
-        var newPlayer = $('#player-input')
-            .val()
-            .trim();
+    $('#add-player').on(
+        'click',
+        function(
+            event
+        ) {
+            event.preventDefault();
 
+            var player = $('#player-input')
+                .val()
+                .trim();
 
-        players.push(newPlayer);
+            players.push(player);
 
+            /* CALLING renderButtons(); HERE WILL MAKE SURE THE NEW PLAYER'S BUTTON HAS BEEN CREATED */
 
-        //$('#players-view').empty();
-
-    });
+            renderButtons();
+        }
+    );
 
     /* AJAX REQUEST TO GET GIFS OF PEOPLE WHEN BUTTONS ARE CLICKED */
 
     function displayGreatestPlayers() {
         var person = $(this).attr('data-name');
-        var queryURL =
-            'https://api.giphy.com/v1/gifs/search?q=' +
-            person +
-            '&api_key=SrsCEPVeiqgySRa6p1rVSatJAA8qOBGD&limit=10';
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + person + '&api_key=SrsCEPVeiqgySRa6p1rVSatJAA8qOBGD&limit=10';
 
-        $.ajax({ url: queryURL, method: 'GET' }).done(function(response) {
-            var results = response.data;
+        $.ajax({
+            url: queryURL,
+            method: 'GET',
+        }).done(
+            function(
+                response
+            ) {
+                var results = response.data;
 
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div class='item'>");
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div class='item'>");
 
-                var rating = results[i].rating;
+                    var rating = results[i].rating;
 
-                var title = results[i].title;
+                    var title = results[i].title;
 
-                var p = $('<p>').text('GIF Title: ' + title + ' & Rating: ' + rating);
-                p.css('font-size', '25px');
+                    var p = $('<p>').text('GIF Title: ' + title + ' & Rating: ' + rating);
+                    p.css('font-size', '25px');
 
-                var personImage = $('<img>').addClass('gif');
-                personImage.attr('src', results[i].images.fixed_height_still.url);
-                personImage.attr(
-                    'data-still',
-                    results[i].images.fixed_height_still.url
-                );
-                personImage.attr('data-animate', results[i].images.fixed_height.url);
+                    var personImage = $('<img>').addClass('gif');
+                    personImage.attr('src', results[i].images.fixed_height_still.url);
+                    personImage.attr('data-still', results[i].images.fixed_height_still.url);
+                    personImage.attr('data-animate', results[i].images.fixed_height.url);
 
-                gifDiv.prepend(p);
-                gifDiv.prepend(personImage);
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(personImage);
 
-                $('#gifs-appear-here').prepend(gifDiv);
-            }
-
-            /* PAUSING AND UNPAUSING GIFS */
-            $('.gif').on('click', function() {
-
-
-                var state = $(this).attr('data-state');
-                console.log(state);
-
-
-                if (state === 'still') {
-                    $(this).attr('src', $(this).attr('data-animate'));
-                    $(this).attr('data-state', 'animate');
-                } else {
-                    $(this).attr('src', $(this).attr('data-still'));
-                    $(this).attr('data-state', 'still');
+                    $('#gifs-appear-here').prepend(gifDiv);
                 }
-            });
-        });
+
+                /* PAUSING AND UNPAUSING GIFS */
+                $('.gif').on('click', function() {
+                    var state = $(this).attr('data-state');
+                    //console.log(state);
+
+                    if (state === 'still') {
+                        $(this).attr('src', $(this).attr('data-animate'));
+                        $(this).attr('data-state', 'animate');
+                    } else {
+                        $(this).attr('src', $(this).attr('data-still'));
+                        $(this).attr('data-state', 'still');
+                    }
+                });
+            }
+        );
     }
+
     $(document).on('click', 'button', displayGreatestPlayers);
+
 });
